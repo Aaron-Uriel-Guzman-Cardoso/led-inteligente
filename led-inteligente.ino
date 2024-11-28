a447f728b70807f6d55433ecd0f9a1e3599b17b4
ialaniss <miriamra056@hotmail.com>
2024-11-28 10:41:40 -0600

    Probar el spiffs y cargar sitio web

--- led-inteligente.ino
+++ led-inteligente.ino
@@ -1,5 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 #include <math.h>
+/*#include "SPIFFS.h"*/
 
 #define PIN_SENSOR 2
 #define PIN_ARRAY 32
@@ -8,14 +9,30 @@ Adafruit_NeoPixel array = Adafruit_NeoPixel(64,PIN_ARRAY,NEO_GRB + NEO_KHZ800);
 
 
 void setup() {
+   Serial.begin(115200);
+ /* if(!SPIFFS.begin(true)){
+    Serial.println("Error");
+    return;
+  }
+  File file= SPIFFS.open("/prueba.txt");
+  if(!file) {
+    Serial.println("Error dos");
+    return;
+    }
+    Serial.println("File content:"); 
+  while(file.available()){
+    Serial.write(file.read()); }
+    */
+  
   array.begin();
   array.show(); //Muestra en la tira lo que antes se haya configurado colores o matriz limpia
   pinMode(PIN_SENSOR,INPUT);
   pinMode(PIN_SON, INPUT);
-  Serial.begin(115200);
+ 
 }
 
 void loop() {
+  
   while(digitalRead(PIN_SON) == LOW && digitalRead(PIN_SENSOR) == LOW);  //Semantiene ahí si no hay movimiento o sonido, no enciende
   if(digitalRead(PIN_SON)==HIGH || digitalRead(PIN_SENSOR) == HIGH) //Enciende si hay sonido o movimiento
   {  
@@ -31,5 +48,6 @@ void loop() {
     delay(2000);
     array.clear();  //comando para apagar todos los leds, limpiar matriz
     array.show();
+    
   }
 }
